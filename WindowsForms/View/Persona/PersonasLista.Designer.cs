72081638719288c17394c990710ab4670d3194e2
alejandro.e87@gmail.com <alejandro.e87@gmail.com>
2024-12-07 10:32:33 -0300

    Persona Lista

--- WindowsForms/View/Persona/PersonasLista.Designer.cs
+++ WindowsForms/View/Persona/PersonasLista.Designer.cs
@@ -38,17 +38,20 @@
             // personaDataGrid
             // 
             personaDataGrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            personaDataGrid.Location = new Point(34, 33);
+            personaDataGrid.Location = new Point(39, 44);
+            personaDataGrid.Margin = new Padding(3, 4, 3, 4);
             personaDataGrid.Name = "personaDataGrid";
-            personaDataGrid.Size = new Size(754, 290);
-            personaDataGrid.TabIndex = 0;
+            personaDataGrid.RowHeadersWidth = 51;
             personaDataGrid.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            personaDataGrid.Size = new Size(862, 387);
+            personaDataGrid.TabIndex = 0;
             // 
             // btn_agregar
             // 
-            btn_agregar.Location = new Point(518, 339);
+            btn_agregar.Location = new Point(592, 452);
+            btn_agregar.Margin = new Padding(3, 4, 3, 4);
             btn_agregar.Name = "btn_agregar";
-            btn_agregar.Size = new Size(75, 23);
+            btn_agregar.Size = new Size(86, 31);
             btn_agregar.TabIndex = 1;
             btn_agregar.Text = "Agregar";
             btn_agregar.UseVisualStyleBackColor = true;
@@ -56,9 +59,10 @@
             // 
             // btn_modificar
             // 
-            btn_modificar.Location = new Point(619, 339);
+            btn_modificar.Location = new Point(707, 452);
+            btn_modificar.Margin = new Padding(3, 4, 3, 4);
             btn_modificar.Name = "btn_modificar";
-            btn_modificar.Size = new Size(75, 23);
+            btn_modificar.Size = new Size(86, 31);
             btn_modificar.TabIndex = 2;
             btn_modificar.Text = "Modificar";
             btn_modificar.UseVisualStyleBackColor = true;
@@ -66,9 +70,10 @@
             // 
             // btn_eliminar
             // 
-            btn_eliminar.Location = new Point(713, 339);
+            btn_eliminar.Location = new Point(815, 452);
+            btn_eliminar.Margin = new Padding(3, 4, 3, 4);
             btn_eliminar.Name = "btn_eliminar";
-            btn_eliminar.Size = new Size(75, 23);
+            btn_eliminar.Size = new Size(86, 31);
             btn_eliminar.TabIndex = 3;
             btn_eliminar.Text = "Eliminar";
             btn_eliminar.UseVisualStyleBackColor = true;
@@ -76,15 +81,16 @@
             // 
             // PersonasLista
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(914, 600);
             Controls.Add(btn_eliminar);
             Controls.Add(btn_modificar);
             Controls.Add(btn_agregar);
             Controls.Add(personaDataGrid);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "PersonasLista";
-            Text = "Form1";
+            Text = "Persona Lista";
             Load += PersonasLista_Load;
             ((System.ComponentModel.ISupportInitialize)personaDataGrid).EndInit();
             ResumeLayout(false);
